de4e1b23088bd2e2bf9520e553278265e8f9c941
Adegbola Adeniyi <adeniyi.4568@yahoo.com>
2022-12-13 19:30:31 -0800

    Write a function that checks for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _islower - Check Holberton
+ * _isalpha - Check Holberton
  * @c: An input character
- * Description: function uses _putchar function to print
- * alphabet in lowercase 10 times
- * Return: 1 if is lowercase or 0 if is uppercase
+ * Description: function returns 1 if the character is a
+ * letter, lowercase or uppercase.
+ * Return: 1 or 0 in otherwise.
  */
 
 int _isalpha(int c)
